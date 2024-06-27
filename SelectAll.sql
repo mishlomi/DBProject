2eaf8de5c2c32e81b55104a499567a5329f9966a
mishlomi <79994896+mishlomi@users.noreply.github.com>
2024-06-27 12:03:46 +0300

    Add files via upload

---
+++ SelectAll.sql
@@ -0,0 +1,10 @@
+  
+select * from book;
+select * from bookcoppy;
+select * from booksales;
+select * from worker;
+select * from customer;
+select * from membership;
+select * from purchase;
+
+commit;
